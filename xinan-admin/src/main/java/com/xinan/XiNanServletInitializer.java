8e1ae08d0f356743c058de65df2b8628616122bb
xinan-dn <xinan.dn@gmail.com>
2023-04-27 14:50:45 +0800

    若依后台管理

---
+++ xinan-admin/src/main/java/com/xinan/XiNanServletInitializer.java
@@ -0,0 +1,18 @@
+package com.xinan;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author ruoyi
+ */
+public class XiNanServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(XiNanApplication.class);
+    }
+}
